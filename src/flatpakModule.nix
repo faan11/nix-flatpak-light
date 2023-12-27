820f4887849e78009f3202511ea89f7c48ed7cdc
Fabio Pagnotta <delphibit@gmail.com>
2023-12-27 11:16:19 +0100

    inline config

--- src/flatpakModule.nix
+++ src/flatpakModule.nix
@@ -1,22 +1,21 @@
-{ lib, pkgs, types, ... }:
-
+{ lib, pkgs, types, ... }@args:
 let
   options = {
     allowFlatpak = {
       default = false;
-      type = types.bool;
+      type = args.types.bool;
       description = "Enable Flatpak support";
     };
 
     flatpakPackages = {
       default = [];
-      type = types.listOf (types.attrsOf types.str);
+      type = args.types.listOf (args.types.attrsOf args.types.str);
       description = "List of Flatpak packages to install/update";
     };
     
     flatpakRemotes = {
       default = [];
-      type = types.listOf types.string;
+      type = args.types.listOf args.types.string;
       description = "List of Flatpak remotes";
     };
   };
@@ -24,28 +23,26 @@ let
 in {
   options = options;
 
-  config = { config, lib, pkgs, ... }:
-
-  if config.allowFlatpak then {
+  config = mkIf args.config.allowFlatpak && has args.config.flatpakPackages && has args.config.flatpakRemotes then {
     services.flatpak = {
       enable = true;
-      extraRemotes = config.flatpakRemotes;
+      extraRemotes = args.config.flatpakRemotes;
     };
 
     # Function to install/update Flatpak packages and set permissions
     programs.flatpak = {
       enable = true;
-      packages = map (pkg: pkgs.flatpakPackages."${pkg.name}" or pkgs.flatpakPackages.${pkg.name}) config.flatpakPackages;
+      packages = map (pkg: pkgs.flatpakPackages."${pkg.name}" or pkgs.flatpakPackages.${pkg.name}) args.config.flatpakPackages;
 
       postInstall = ''
-        for p in ${config.flatpakPackages}; do
+        for p in ${args.config.flatpakPackages}; do
           flatpak install -y $p.repo $p.name
           flatpak override --$p.user --app $p.name --$p.permissions
         done
       '';
 
       postUpdate = ''
-        for p in ${config.flatpakPackages}; do
+        for p in ${args.config.flatpakPackages}; do
           flatpak update -y $p.name
           flatpak override --$p.user --app $p.name --$p.permissions
         done
