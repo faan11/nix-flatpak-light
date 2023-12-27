03eb0537b13ad4bf9fd2e14dcdebbbea7699d987
Fabio Pagnotta <delphibit@gmail.com>
2023-12-27 11:58:35 +0100

    Fixs line 39 and 44

--- src/flatpakModule.nix
+++ src/flatpakModule.nix
@@ -36,12 +36,12 @@ let
       packages = map (pkg: pkgs.flatpakPackages."${pkg.name}" or pkgs.flatpakPackages.${pkg.name}) config.flatpakPackages;
 
       postInstall = ''
-        + lib.concatMapStringsSep " && " (pkg: ''
+        + lib.concatMapStringsSep (pkg: ''
           "${pkgs.flatpak}/bin/flatpak install -y ${pkg.repo} ${pkg.name} && ${pkgs.flatpak}/bin/flatpak override --user=${pkg.user} --app=${pkg.name} -- ${pkg.permissions}"
         '') config.flatpakPackages;
 
       postUpdate = ''
-        + lib.concatMapStringsSep " && " (pkg: ''
+        + lib.concatMapStringsSep (pkg: ''
           "${pkgs.flatpak}/bin/flatpak update -y ${pkg.name} && ${pkgs.flatpak}/bin/flatpak override --user=${pkg.user} --app=${pkg.name} -- ${pkg.permissions}"
         '') config.flatpakPackages;
     };
