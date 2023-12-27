01b78d6b5786a2b6362547bdc6a573c777125721
Fabio Pagnotta <delphibit@gmail.com>
2023-12-27 12:02:21 +0100

    Fixs post update

--- src/flatpakModule.nix
+++ src/flatpakModule.nix
@@ -35,16 +35,15 @@ let
       enable = true;
       packages = map (pkg: pkgs.flatpakPackages."${pkg.name}" or pkgs.flatpakPackages.${pkg.name}) config.flatpakPackages;
 
-      postInstall = ''
-        + lib.concatMapStringsSep (pkg: ''
-          "${pkgs.flatpak}/bin/flatpak install -y ${pkg.repo} ${pkg.name} && ${pkgs.flatpak}/bin/flatpak override --user=${pkg.user} --app=${pkg.name} --${pkg.permissions}"
-        '') config.flatpakPackages;
+    postInstall = ''
+      + lib.concatMapStringsSep (flatpakPackage: ''
+        "${pkgs.flatpak}/bin/flatpak install -y ${flatpakPackage.repo} ${flatpakPackage.name} && ${pkgs.flatpak}/bin/flatpak override --user=${flatpakPackage.user} --app=${flatpakPackage.name} --${flatpakPackage.permissions}"
+      '') config.flatpakPackages;
 
-
-      postUpdate = ''
-    + lib.concatMapStringsSep (flatpakPackage: ''
-      "${pkgs.flatpak}/bin/flatpak update -y ${flatpakPackage.name} && ${pkgs.flatpak}/bin/flatpak override --user=${flatpakPackage.user} --app=${flatpakPackage.name} --${flatpakPackage.permissions}"
-    '') config.flatpakPackages;    };
+    postUpdate = ''
+      + lib.concatMapStringsSep (flatpakPackage: ''
+        "${pkgs.flatpak}/bin/flatpak update -y ${flatpakPackage.name} && ${pkgs.flatpak}/bin/flatpak override --user=${flatpakPackage.user} --app=${flatpakPackage.name} --${flatpakPackage.permissions}"
+      '') config.flatpakPackages;
   } else {};
 
 in {
